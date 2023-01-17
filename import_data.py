88d85f63564e884c1b1e43217ca31ecb175e4600
Moos Castelijn <mooscastelijn@gmail.com>
2023-01-17 12:36:30 +0100

    uitleg over synch in readme, import_data beetje werk

--- import_data.py
+++ import_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+pd.set_option('display.max_colwidth', None)
 
 food_loss = pd.read_csv('Food Loss Data.csv')
 temperature = pd.read_csv('matYearCountry.csv')
@@ -11,13 +12,10 @@ food_loss.head(5)
 countries = food_loss["country"]
 
 # see which year has the most information
-data2022 = food_loss[food_loss['year'] == 2021]
-data_per_year = {}
-for year in food_loss['year'].unique():
-    data_per_year[year] = data2022 = food_loss[food_loss['year'] == year]
-    print(f'the year {year} has {data_per_year[year].shape[0]} datapoints')
+counts = food_loss['year'].value_counts()
+counts[counts > 100]
 
-data2022.shape[0] #nb of rows
+food_loss['year'].shape[0] #nb of rows
 
 print(countries.unique())
 
