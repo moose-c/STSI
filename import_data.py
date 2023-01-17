ba018859f910ce30ea0f2e6c13eba89d54c16d5e
Moos Castelijn <mooscastelijn@gmail.com>
2023-01-17 11:24:34 +0100

    .gitignore bijgewerkt

--- import_data.py
+++ import_data.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 food_loss = pd.read_csv('Food Loss Data.csv')
 temperature = pd.read_csv('matYearCountry.csv')
 
+
 food_loss.plot()
 countries = food_loss["country"]
 print(countries.unique())
