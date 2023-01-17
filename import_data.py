ebbce8ff6f41cd58256aa448f80d9b0de414073c
Moos Castelijn <mooscastelijn@gmail.com>
2023-01-17 11:15:24 +0100

    Initial commit

---
+++ import_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+food_loss = pd.read_csv('Food Loss Data.csv')
+temperature = pd.read_csv('matYearCountry.csv')
+
+food_loss.plot()
+countries = food_loss["country"]
+print(countries.unique())
+plt.show()
+# https://datahub.io/core/global-temp#python
---
+++ readme.md
@@ -0,0 +1,2 @@
+Food loss data from: https://www.fao.org/platform-food-loss-waste/flw-data/en
+Temperature data from: https://www.kaggle.com/code/akshaychavan/average-temperature-per-country-per-year/data?select=GlobalTemperatures.csv
