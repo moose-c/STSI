88efe261b90cde6934a2c0c77a6e63309528604f
martboersma <m.w.boersma@students.uu.nl>
2023-01-19 13:00:22 +0100

    13:00

--- import_data_Mart.py
+++ import_data_Mart.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_colwidth', None)
 
 
-food_loss = pd.read_csv('Food Loss Data.csv')
+food_loss = pd.read_csv('Food Loss Data.csv', index_col=1)
 temperature = pd.read_csv('matYearCountry.csv')
-GDP = pd.read_csv('global_gdp.csv')
+GDP = pd.read_csv('global_gdp.csv', index_col=0)
 
 # print(GDP.head(5))
  
 food_loss.head(5)
-countries = food_loss["country"]
+#countries = food_loss["country"]
 
 # see which year has the most information
 counts = food_loss['year'].value_counts()
@@ -25,20 +25,32 @@ food_loss['year'].shape[0] #nb of rows
 counts=GDP['2021'].value_counts()
 counts
 
-GDP2021= pd.DataFrame()
-GDP2021['Country']=GDP['Country Name']
-GDP2021['2021']=GDP['2021']
-print(GDP2021.head())
+#GDP2021= pd.DataFrame()
+#GDP2021['Country']=GDP['Country Name']
+#GDP2021['2021']=GDP['2021']
+#print(GDP2021.head())
 
 food_loss_whole_chain = food_loss[food_loss['food_supply_stage'] == "Whole supply chain"]
-data = pd.DataFrame(columns=["country", "year", "loss_percentage", "GDP", "temperature"])
-for index, row in food_loss_whole_chain.iterrows():
-    addition = {}
-    addition["country"] = row["country"]
-    addition["year"] = row["year"]
-    addition["loss_percentage"] = row["loss_percentage"]
-   # addition["temperature"] = temperature[addition["country"]][addition["year"]] # nu maar tot 2015
-    addition["GDP"] = GDP2021[addition["country"]][addition['2021']]
-    data.append(addition)
+#print(food_loss_whole_chain['loss_percentage'].head())
 
-print(data.head())
+
+#data = pd.DataFrame(columns=[ "year", "loss_percentage", "GDP", "temperature"])
+#for index, row in food_loss_whole_chain.iterrows():
+    #addition = {}
+    
+    #addition["year"] = row["year"]
+    #addition["loss_percentage"] = row["loss_percentage"]
+   # addition["temperature"] = temperature[addition["country"]][addition["year"]] # nu maar tot 2015
+    #addition["GDP"] = GDP[addition['year']]
+   
+    #data.append(addition)
+
+FL=food_loss_whole_chain.join(GDP['2021'])
+FL['GDP2021']=FL['2021']
+
+fl2=food_loss_whole_chain
+fl2=fl2[fl2.year == 2002]
+
+FL2=fl2.join(GDP['2002'])
+print(FL2)
+FL2.to_csv('out.csv')
